abe456cbf16a26262caaad47c6d2456b97ba22ed
Manoj <manojhn036@gmail.com>
2023-07-22 20:00:39 +0530

    first commit

---
+++ src/services/api-client.ts
@@ -0,0 +1,36 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "f98f1d35c07640e7b2b5033f9c3df5b8",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
